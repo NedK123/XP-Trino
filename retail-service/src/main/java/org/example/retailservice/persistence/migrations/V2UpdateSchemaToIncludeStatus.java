f98d961c13e562c5456c3ebf2bdde08ef5a14aae
naderkahwaji <nader.kahwaji@gmail.com>
2025-04-20 02:49:07 +0300

    supporting mongock rollbacks using schema versions

--- retail-service/src/main/java/org/example/retailservice/persistence/migrations/V2UpdateSchemaToIncludeStatus.java
+++ retail-service/src/main/java/org/example/retailservice/persistence/migrations/V2UpdateSchemaToIncludeStatus.java
@@ -1,12 +1,10 @@
 package org.example.retailservice.persistence.migrations;
 
 import com.mongodb.client.model.Filters;
-import com.mongodb.client.model.Updates;
 import io.mongock.api.annotations.ChangeUnit;
 import io.mongock.api.annotations.Execution;
 import io.mongock.api.annotations.RollbackExecution;
 import lombok.AllArgsConstructor;
-import org.bson.Document;
 import org.example.retailservice.persistence.schemas.SchemaGenerator;
 import org.springframework.data.mongodb.core.MongoTemplate;
 
@@ -26,10 +24,6 @@ public class V2UpdateSchemaToIncludeStatus {
   public void rollback() {
     mongoTemplate
         .getCollection("_schema")
-        .updateOne(
-            Filters.eq("table", "retail_records"), // Query to find the document
-            Updates.pull(
-                "fields", new Document("name", "status")) // Use $pull to remove the "status" field
-            );
+        .replaceOne(Filters.eq("table", "retail_records"), SchemaGenerator.generate(1).asDoc());
   }
 }
